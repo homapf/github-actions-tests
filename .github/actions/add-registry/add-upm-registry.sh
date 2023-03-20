dae24a9d476ebf30389b47e42a7387aa617a9f16
Paul FIGIEL <pf@homagames.com>
2023-03-20 17:35:03 +0100

    Test

--- .github/actions/add-registry/add-upm-registry.sh
+++ .github/actions/add-registry/add-upm-registry.sh
@@ -10,9 +10,9 @@ manifest_file="Packages/manifest.json"
 
 # Check if the registry already exists in the manifest file
 if grep -q "$url" "$manifest_file"; then
-  echo "Registry already exists in manifest file."
+  echo "Registry $name already exists in manifest file."
 else
   # Add the registry to the manifest file
   jq '.scopedRegistries += [{"name":"'"$name"'","url":"'"$url"'","scopes":["'"$scope"'"]}]' $manifest_file > tmp.$$.json && mv tmp.$$.json $manifest_file
-  echo "Registry added to manifest file."
+  echo "Registry $name added to manifest file."
 fi
